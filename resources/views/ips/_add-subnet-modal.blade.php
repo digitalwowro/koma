d21af650f35a38cbf8b29a87c6c58d0fa4f835f8
Tudor <tudor@tudorg.ro>
2017-01-31 19:51:00 +0200

    fix button

--- resources/views/ips/_add-subnet-modal.blade.php
+++ resources/views/ips/_add-subnet-modal.blade.php
@@ -14,7 +14,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary">Add</button>
+                <button type="submit" class="btn btn-primary">Add</button>
             </div>
             {!! Form::close() !!}
         </div>
