0b8a8b52e965bb269588e17d4ba1327287f76a5d
Tudor <tudor@tudorg.ro>
2019-05-16 15:17:24 +0300

    Fix styling

--- resources/views/devices/_save.blade.php
+++ resources/views/devices/_save.blade.php
@@ -2,7 +2,7 @@
     <div class="col-lg-12">
         <div class="main-box clearfix">
             <div class="form-group">
-                <div class="col-lg-12" style="margin-top:20px; margin-bottom:20px; margin-left:170px;">
+                <div class="col-lg-12" style="padding-top:20px; padding-bottom:20px; padding-left:170px;">
                     <button type="submit" class="btn btn-primary">{{ isset($device) ? 'Save' : 'Add' }}</button>
                     <a href="{{ route('devices.index', $deviceSection->id) }}" class="btn btn-default">Cancel</a>
                 </div>
